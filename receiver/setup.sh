016898aad55bfe4245bf9470861e842676890e65
Jio <jio@wldh.org>
2020-01-10 19:48:03 +0900

    original mac notification

--- receiver/setup.sh
+++ receiver/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ip link set wlp1s0 down
+echo "Your MAC address of wlp1s0 temporarily changed to 12:34:56:78:90:ff."
+echo "The original MAC address was " $(cat /sys/class/net/wlp1s0/address) " ."
 ip link set wlp1s0 address 12:34:56:78:90:ff
 ip link set wlp1s0 up
 
