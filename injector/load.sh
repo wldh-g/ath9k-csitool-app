1cc719abede1477996f3dacd5e0481996306afd9
Jio <jio@wldh.org>
2020-01-11 05:13:39 +0900

    scripts stable

--- injector/load.sh
+++ injector/load.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/sudo /bin/bash
 
-ip addr add 10.5.19.1/24 dev wlp1s0 &1>/dev/null &2>/dev/null
-systemctl stop isc-dhcp-server &1>/dev/null &2>/dev/null
+ip addr add 10.5.19.1/24 dev wlp1s0 > /dev/null 2>&1
+systemctl stop isc-dhcp-server > /dev/null 2>&1
 dhcpd -cf ./dhcpd.conf
 hostapd -d ./hostapd.conf -B
 
-echo "Configured."
+echo "Injector Configured."
--- receiver/setup.sh
+++ receiver/setup.sh
@@ -2,14 +2,16 @@
 
 ip link set wlp1s0 down
 echo "Your MAC address of wlp1s0 temporarily changed to 12:34:56:78:90:ff."
-echo "The original MAC address was " $(cat /sys/class/net/wlp1s0/address) " ."
+echo "The original MAC address was "$(cat /sys/class/net/wlp1s0/address)" ."
 ip link set wlp1s0 address 12:34:56:78:90:ff
 ip link set wlp1s0 up
 
 iw wlp1s0 connect Atheros-CSITool
 
-while [ $(iw wlp1s0 info | grep ssid | awk '{print $2}') != "Atheros-CSITool" ]; do
-	iw wlp1s0 connect Atheros-CSITool
+echo "Trying to connect to Injector ..."
+while [ "$(iw wlp1s0 info | grep ssid | awk '{print $2}')" != "Atheros-CSITool" ]; do
+	iw wlp1s0 connect Atheros-CSITool > /dev/null 2>&1
 done
+echo "Connected!"
 
-echo "Configured."
+echo "Receiver Configured."
