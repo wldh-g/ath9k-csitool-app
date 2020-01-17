733f0445e7a80148ae8f55c8841376708e03e70f
Jio <jio@wldh.org>
2020-01-17 14:31:35 +0000

    receiver disconnection notify

--- injector/unload.sh
+++ injector/unload.sh
@@ -5,4 +5,5 @@ kill -TERM $(ps aux | grep 'hostapd'| awk 'NR==1{print $2}')
 
 ip addr del 10.5.19.1/24 dev wlp1s0 > /dev/null 2>&1
 
+echo "Disconnected from all receivers."
 echo "Injector Unloaded."
